c5b492c53d0132a07332e24a11cdcb2b50877ffe
Jacek Gałka <jacgalka@icloud.com>
2019-10-01 10:51:54 +0200

    Updates

---
+++ precache-manifest.ff91deee7c4255034996108a3c7ae04e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b420fc8968febe1b8257636e6fb649ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92d9d232839e5ec8a762",
+    "url": "/static/css/main.142fcfb0.chunk.css"
+  },
+  {
+    "revision": "a352c92f69f0bee7908e",
+    "url": "/static/js/2.c3da4c9d.chunk.js"
+  },
+  {
+    "revision": "92d9d232839e5ec8a762",
+    "url": "/static/js/main.9313fa27.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "424e4b1caea84e5c31eb45df3a939733",
+    "url": "/static/media/filter.424e4b1c.svg"
+  },
+  {
+    "revision": "368161853aa1839fbcffdafbe983b286",
+    "url": "/static/media/list.36816185.svg"
+  },
+  {
+    "revision": "d609e73341d6597e6e7d65891eb18637",
+    "url": "/static/media/logoWhite.d609e733.svg"
+  },
+  {
+    "revision": "0dd8f9e98c7b150f8c9fddfce1451160",
+    "url": "/static/media/plus.0dd8f9e9.svg"
+  },
+  {
+    "revision": "c4a72c3058c0b08278e3ca1c2af7e033",
+    "url": "/static/media/search.c4a72c30.svg"
+  },
+  {
+    "revision": "bceb818b3015bfe35e61d0bca069fefb",
+    "url": "/static/media/user.bceb818b.svg"
+  }
+]);
